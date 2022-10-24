36e99cc87d291ed7d916ae96181f857bc1868dff
Oleksandr <114135649+Qwedcy@users.noreply.github.com>
2022-10-24 12:10:48 +0300

    Initial commit

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main()
+{
+    double a, b, c, d, f, g;
+    cout << "Input 2 number:";
+    cin >> a >> b;
+    c = a + b;
+    d = a - b;
+    f = a / b;
+    g = a * b;
+    cout << "a+b=" << c << std::endl;
+    cout << "a-b=" << d << std::endl;
+    cout << "a/b=" << f << std::endl;
+    cout << "a*b=" << g << std::endl;
+    system("pause");
+}
